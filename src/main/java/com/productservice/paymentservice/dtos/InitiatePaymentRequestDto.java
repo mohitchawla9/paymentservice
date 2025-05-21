f18a5c94371078447babe20bbef31412e022b33c
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-21 16:27:40 +0530

    Integrating RazorPay Payment Gateway

---
+++ src/main/java/com/productservice/paymentservice/dtos/InitiatePaymentRequestDto.java
@@ -0,0 +1,13 @@
+package com.productservice.paymentservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InitiatePaymentRequestDto {
+    private String orderId;
+    private Long amount;
+    private String phoneNumber;
+    private String email;
+}
