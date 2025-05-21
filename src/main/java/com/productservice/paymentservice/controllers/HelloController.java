d4b0ead1661d5b9da80895d4cab2364f16ec6639
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-21 13:47:03 +0530

    Starting Payment Service

---
+++ src/main/java/com/productservice/paymentservice/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.productservice.paymentservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello Class";
+    }
+}
